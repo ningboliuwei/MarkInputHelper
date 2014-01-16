4e5ed049493832a80abf82356f0a480225487ad9
ningboliuwei <liuwei@nbu.edu.cn>
2014-01-16 20:53:02 +0800

    完成基本功能

--- MarkInputHelper/frmMain.Designer.cs
+++ MarkInputHelper/frmMain.Designer.cs
@@ -51,11 +51,11 @@
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(3, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(669, 102);
+			this.label1.Size = new System.Drawing.Size(705, 102);
 			this.label1.TabIndex = 3;
-			this.label1.Text = "说明：\r\n第一步：打开成绩输入页面\r\n第二步：选择成绩输入模式\r\n第三步：在EXCEL中选中需要输入的数据（只是成绩，不包含表头），并按Ctrl+V或点击“粘贴”" +
-    "按钮，将输入粘贴到表格中\r\n第四步：点击“输入”按钮，并按提示进行操作\r\n注意：1. 暂不支持带实验成绩的模式 2. 若要输入新的成绩，先点击“清空”按钮，再进" +
-    "行粘贴";
+			this.label1.Text = "说明：\r\n第一步：打开成绩输入页面\r\n第二步：选择成绩输入模式\r\n第三步：在EXCEL中选中并复制需要输入的数据（只是成绩，不包含表头），并按Ctrl+V或点击“" +
+    "粘贴”按钮，将输入粘贴到表格中\r\n第四步：点击“输入”按钮，并按提示进行操作\r\n注意：1. 暂不支持带实验成绩的模式 2. 若要输入新的成绩，先点击“清空”按钮" +
+    "，再进行粘贴";
 			// 
 			// tableLayoutPanel1
 			// 
@@ -73,7 +73,7 @@
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(696, 501);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(841, 501);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// gdvMain
@@ -83,7 +83,7 @@
 			this.gdvMain.Location = new System.Drawing.Point(3, 148);
 			this.gdvMain.Name = "gdvMain";
 			this.gdvMain.RowTemplate.Height = 24;
-			this.gdvMain.Size = new System.Drawing.Size(690, 300);
+			this.gdvMain.Size = new System.Drawing.Size(835, 300);
 			this.gdvMain.TabIndex = 6;
 			// 
 			// tableLayoutPanel2
@@ -100,15 +100,15 @@
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
 			this.tableLayoutPanel2.RowCount = 1;
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(690, 44);
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(835, 44);
 			this.tableLayoutPanel2.TabIndex = 4;
 			// 
 			// btnClear
 			// 
 			this.btnClear.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.btnClear.Location = new System.Drawing.Point(463, 3);
+			this.btnClear.Location = new System.Drawing.Point(559, 3);
 			this.btnClear.Name = "btnClear";
-			this.btnClear.Size = new System.Drawing.Size(224, 38);
+			this.btnClear.Size = new System.Drawing.Size(273, 38);
 			this.btnClear.TabIndex = 2;
 			this.btnClear.Text = "清空(&C)";
 			this.btnClear.UseVisualStyleBackColor = true;
@@ -117,9 +117,9 @@
 			// btnInput
 			// 
 			this.btnInput.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.btnInput.Location = new System.Drawing.Point(233, 3);
+			this.btnInput.Location = new System.Drawing.Point(281, 3);
 			this.btnInput.Name = "btnInput";
-			this.btnInput.Size = new System.Drawing.Size(224, 38);
+			this.btnInput.Size = new System.Drawing.Size(272, 38);
 			this.btnInput.TabIndex = 1;
 			this.btnInput.Text = "输入(&I)";
 			this.btnInput.UseVisualStyleBackColor = true;
@@ -130,7 +130,7 @@
 			this.btnPaste.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.btnPaste.Location = new System.Drawing.Point(3, 3);
 			this.btnPaste.Name = "btnPaste";
-			this.btnPaste.Size = new System.Drawing.Size(224, 38);
+			this.btnPaste.Size = new System.Drawing.Size(272, 38);
 			this.btnPaste.TabIndex = 0;
 			this.btnPaste.Text = "粘贴(&P)";
 			this.btnPaste.UseVisualStyleBackColor = true;
@@ -150,16 +150,16 @@
 			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
 			this.tableLayoutPanel3.RowCount = 1;
 			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel3.Size = new System.Drawing.Size(690, 29);
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(835, 29);
 			this.tableLayoutPanel3.TabIndex = 7;
 			// 
 			// radioButton3
 			// 
 			this.radioButton3.AutoSize = true;
 			this.radioButton3.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.radioButton3.Location = new System.Drawing.Point(463, 3);
+			this.radioButton3.Location = new System.Drawing.Point(559, 3);
 			this.radioButton3.Name = "radioButton3";
-			this.radioButton3.Size = new System.Drawing.Size(224, 23);
+			this.radioButton3.Size = new System.Drawing.Size(273, 23);
 			this.radioButton3.TabIndex = 2;
 			this.radioButton3.TabStop = true;
 			this.radioButton3.Text = "同时输入平时与期末成绩";
@@ -169,9 +169,9 @@
 			// 
 			this.radioButton2.AutoSize = true;
 			this.radioButton2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.radioButton2.Location = new System.Drawing.Point(233, 3);
+			this.radioButton2.Location = new System.Drawing.Point(281, 3);
 			this.radioButton2.Name = "radioButton2";
-			this.radioButton2.Size = new System.Drawing.Size(224, 23);
+			this.radioButton2.Size = new System.Drawing.Size(272, 23);
 			this.radioButton2.TabIndex = 1;
 			this.radioButton2.TabStop = true;
 			this.radioButton2.Text = "只输入期末成绩";
@@ -183,7 +183,7 @@
 			this.radioButton1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.radioButton1.Location = new System.Drawing.Point(3, 3);
 			this.radioButton1.Name = "radioButton1";
-			this.radioButton1.Size = new System.Drawing.Size(224, 23);
+			this.radioButton1.Size = new System.Drawing.Size(272, 23);
 			this.radioButton1.TabIndex = 0;
 			this.radioButton1.TabStop = true;
 			this.radioButton1.Text = "只输入平时成绩";
@@ -193,7 +193,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(696, 501);
+			this.ClientSize = new System.Drawing.Size(841, 501);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
